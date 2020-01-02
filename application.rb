cf5853204191b7183994f6425295d9d146bd9018
Phil Billingsby <philbillingsby@gmail.com>
2020-01-02 18:17:56 -0500

    Done.

--- application.rb
+++ application.rb
@@ -2,8 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    resp.write "Hello, my name is"
+    resp.finish 
   end
 
 end
